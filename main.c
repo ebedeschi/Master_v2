5405d19b0bdf5d6a204f37ee42ef67cc77d6fe8a
Emanuele <Emanuele@Danilo-Desktop.deis.unibo.it>
2016-02-26 10:35:19 +0100

    ADC prova

--- main.c
+++ main.c
@@ -53,7 +53,7 @@
 #define MAXSLAVE	3
 #define TIMEOUTSLAVE	2
 #define RTCSECONDS_START	15
-#define RTCSECONDS_AFTER	15
+#define RTCSECONDS_AFTER	900
 
 	Modem_Config1 Modem_Config1_Struct;
 	Modem_Config2 Modem_Config2_Struct;
@@ -578,9 +578,68 @@ char buffer[BUFFER_LENGHT+1];
 int i=0, c=0;
 unsigned long count_tx=0;
 unsigned long count_tx_irq=0;
-
+unsigned long hum=0;
 
 int main(void) {
+
+//	WDTCTL = WDTPW | WDTHOLD;                 // Stop WDT
+//	initRadioGPIO();
+//	// GPIO Setup
+//	P1OUT &= ~BIT0;                           // Clear LED to start
+//	P1DIR |= BIT0;                            // P1.0 output
+//	P3SEL1 |= BIT0;                           // Configure P1.1 for ADC
+//	P3SEL0 |= BIT0;
+//
+//	// Disable the GPIO power-on default high-impedance mode to activate
+//	// previously configured port settings
+//	PM5CTL0 &= ~LOCKLPM5;
+//
+//	// By default, REFMSTR=1 => REFCTL is used to configure the internal reference
+//	while(REFCTL0 & REFGENBUSY);              // If ref generator busy, WAIT
+//	REFCTL0 |= REFVSEL_0 | REFON;             // Select internal ref = 1.2V
+//											// Internal Reference ON
+//
+//	// Configure ADC12
+//	ADC12CTL0 = ADC12SHT0_2 | ADC12ON;
+//	ADC12CTL1 = ADC12SHP;                     // ADCCLK = MODOSC; sampling timer
+//	ADC12CTL2 |= ADC12RES_2;                  // 12-bit conversion results
+//	ADC12IER0 |= ADC12IE0;                    // Enable ADC conv complete interrupt
+//	ADC12MCTL0 |= ADC12INCH_12 | ADC12VRSEL_12; // A1 ADC input select; Vref=1.2V
+//
+//	while(!(REFCTL0 & REFGENRDY));            // Wait for reference generator
+//											// to settle
+//
+//	while(1)
+//	{
+//	__delay_cycles(5000000);                    // Delay between conversions
+//	ADC12CTL0 |= ADC12ENC | ADC12SC;         // Sampling and conversion start
+//
+//	__bis_SR_register(LPM0_bits + GIE);      // LPM0, ADC10_ISR will force exit
+//	__no_operation();                        // For debug only
+//
+//	initSPIA0();
+//	radioON();
+//	radioInit();
+//
+//	packet[0]='\0';
+//	sprintf(packet, "H1:%lu:%d:%d", hum, (Pa_Config_Struct.PaSelect==PaSelect_PA_Boost_Pin)?1:0, Pa_Config_Struct.OutputPower);
+////		sprintf(packet, "T5:%.2f:%d:%d", g_temp, (Pa_Config_Struct.PaSelect==PaSelect_PA_Boost_Pin)?1:0, Pa_Config_Struct.OutputPower);
+//	//		sprintf(packet, "T3:%.2f", g_temp);
+//	PayLoadLenghtSet_Value=strlen(packet);
+//	Packet_Set(PreambleLenghtSet_Value,PayLoadLenghtSet_Value);
+//	P3OUT ^= BIT7;                      // Toggle P3.7 using exclusive-OR
+//	Packet_Tx(PayLoadLenghtSet_Value, packet);
+//	count_tx++;
+//
+//	__bis_SR_register(LPM0_bits + GIE);
+//	P3OUT ^= BIT7;                      // Toggle P3.7 using exclusive-OR
+//
+//	radioOFF();
+//	disableSPIA0();
+//
+//	}
+
+
 	WDTCTL = WDTPW | WDTHOLD;       // Stop WDT
 
 	rtc_seconds = RTCSECONDS_START;
@@ -592,7 +651,7 @@ int main(void) {
 	setShoutDownModeRS485();
 
 
-	//------ MCLK -------
+//	//------ MCLK -------
 //	// Disable the GPIO power-on default high-impedance mode to activate
 //	// previously configured port settings
 //	PM5CTL0 &= ~LOCKLPM5;
@@ -652,11 +711,16 @@ int main(void) {
 
     	stopRTC();
 
+		if(count>120)
+				rtc_seconds = RTCSECONDS_AFTER;
+
     	initI2CB0();
 		// Read Temperature
 //		SHT21ReadTemperature();
 		g_temp = g_temp;
 		disableI2CB0();
+
+		g_temp = 6.78;
 
 		// random start delay for next
 		int start = ((float)g_temp - (int)g_temp) * 1000;
@@ -671,10 +735,12 @@ int main(void) {
     	radioInit();
 
 		packet[0]='\0';
-		sprintf(packet, "T5:%.2f:%d:%d", g_temp, (Pa_Config_Struct.PaSelect==PaSelect_PA_Boost_Pin)?1:0, Pa_Config_Struct.OutputPower);
+		sprintf(packet, "T7:%.2f:%d:%d", g_temp, (Pa_Config_Struct.PaSelect==PaSelect_PA_Boost_Pin)?1:0, Pa_Config_Struct.OutputPower);
 		//		sprintf(packet, "T3:%.2f", g_temp);
 		PayLoadLenghtSet_Value=strlen(packet);
 		Packet_Set(PreambleLenghtSet_Value,PayLoadLenghtSet_Value);
+		unsigned short pr_l = 0, pa_l = 0;
+		Packet_Get(&pr_l, &pr_l);
 		P3OUT ^= BIT7;                      // Toggle P3.7 using exclusive-OR
 		Packet_Tx(PayLoadLenghtSet_Value, packet);
     	count_tx++;
@@ -873,4 +939,62 @@ __interrupt void RTC_ISR(void)
         case RTCIV_RT1PSIFG:  break;        // RT1PSIFG
         default: break;
     }
+}
+
+#if defined(__TI_COMPILER_VERSION__) || defined(__IAR_SYSTEMS_ICC__)
+#pragma vector = ADC12_VECTOR
+__interrupt void ADC12_ISR(void)
+#elif defined(__GNUC__)
+void __attribute__ ((interrupt(ADC12_VECTOR))) ADC12_ISR (void)
+#else
+#error Compiler not supported!
+#endif
+{
+  switch (__even_in_range(ADC12IV, ADC12IV_ADC12RDYIFG))
+  {
+    case ADC12IV_NONE:        break;        // Vector  0:  No interrupt
+    case ADC12IV_ADC12OVIFG:  break;        // Vector  2:  ADC12MEMx Overflow
+    case ADC12IV_ADC12TOVIFG: break;        // Vector  4:  Conversion time overflow
+    case ADC12IV_ADC12HIIFG:  break;        // Vector  6:  ADC12BHI
+    case ADC12IV_ADC12LOIFG:  break;        // Vector  8:  ADC12BLO
+    case ADC12IV_ADC12INIFG:  break;        // Vector 10:  ADC12BIN
+    case ADC12IV_ADC12IFG0:                 // Vector 12:  ADC12MEM0 Interrupt
+    	hum=ADC12MEM0;
+      __bic_SR_register_on_exit(LPM0_bits); // Exit active CPU
+      break;                                // Clear CPUOFF bit from 0(SR)
+
+    case ADC12IV_ADC12IFG1:   break;        // Vector 14:  ADC12MEM1
+    case ADC12IV_ADC12IFG2:   break;        // Vector 16:  ADC12MEM2
+    case ADC12IV_ADC12IFG3:   break;        // Vector 18:  ADC12MEM3
+    case ADC12IV_ADC12IFG4:   break;        // Vector 20:  ADC12MEM4
+    case ADC12IV_ADC12IFG5:   break;        // Vector 22:  ADC12MEM5
+    case ADC12IV_ADC12IFG6:   break;        // Vector 24:  ADC12MEM6
+    case ADC12IV_ADC12IFG7:   break;        // Vector 26:  ADC12MEM7
+    case ADC12IV_ADC12IFG8:   break;        // Vector 28:  ADC12MEM8
+    case ADC12IV_ADC12IFG9:   break;        // Vector 30:  ADC12MEM9
+    case ADC12IV_ADC12IFG10:  break;        // Vector 32:  ADC12MEM10
+    case ADC12IV_ADC12IFG11:  break;        // Vector 34:  ADC12MEM11
+    case ADC12IV_ADC12IFG12:  break;
+    case ADC12IV_ADC12IFG13:  break;        // Vector 38:  ADC12MEM13
+    case ADC12IV_ADC12IFG14:  break;        // Vector 40:  ADC12MEM14
+    case ADC12IV_ADC12IFG15:  break;        // Vector 42:  ADC12MEM15
+    case ADC12IV_ADC12IFG16:  break;        // Vector 44:  ADC12MEM16
+    case ADC12IV_ADC12IFG17:  break;        // Vector 46:  ADC12MEM17
+    case ADC12IV_ADC12IFG18:  break;        // Vector 48:  ADC12MEM18
+    case ADC12IV_ADC12IFG19:  break;        // Vector 50:  ADC12MEM19
+    case ADC12IV_ADC12IFG20:  break;        // Vector 52:  ADC12MEM20
+    case ADC12IV_ADC12IFG21:  break;        // Vector 54:  ADC12MEM21
+    case ADC12IV_ADC12IFG22:  break;        // Vector 56:  ADC12MEM22
+    case ADC12IV_ADC12IFG23:  break;        // Vector 58:  ADC12MEM23
+    case ADC12IV_ADC12IFG24:  break;        // Vector 60:  ADC12MEM24
+    case ADC12IV_ADC12IFG25:  break;        // Vector 62:  ADC12MEM25
+    case ADC12IV_ADC12IFG26:  break;        // Vector 64:  ADC12MEM26
+    case ADC12IV_ADC12IFG27:  break;        // Vector 66:  ADC12MEM27
+    case ADC12IV_ADC12IFG28:  break;        // Vector 68:  ADC12MEM28
+    case ADC12IV_ADC12IFG29:  break;        // Vector 70:  ADC12MEM29
+    case ADC12IV_ADC12IFG30:  break;        // Vector 72:  ADC12MEM30
+    case ADC12IV_ADC12IFG31:  break;        // Vector 74:  ADC12MEM31
+    case ADC12IV_ADC12RDYIFG: break;        // Vector 76:  ADC12RDY
+    default: break;
+  }
 }
